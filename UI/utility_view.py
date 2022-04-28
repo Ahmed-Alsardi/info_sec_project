831915304d9ede76fd5853b131058341039e005b
Ahmed AlSardi <A7Dpy@outlook.com>
2022-04-28 15:13:53 +0300

    adding message view

--- UI/utility_view.py
+++ UI/utility_view.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import logging
 from UI.utility import BLACK, WHITE
 from typing import List
+from application_context import UserMessage
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
 
@@ -50,6 +52,35 @@ class SideView(tk.Frame):
         logging.info(f"User {username} clicked")
 
 
+class MessageView(tk.Frame):
+    def __init__(self, parent, width: int, height: int, bg, text_color, message_list: List[UserMessage]):
+        tk.Frame.__init__(self, parent, width=width, height=height, background=bg)
+        self.frame_width = width
+        self.frame_height = height
+        self.bg = bg
+        self.text_color = text_color
+        self.message_list = message_list
+        # create label "my messages"
+        self.label_my_messages = tk.Label(self, text="My Messages", bg=bg, fg=text_color)
+        self.label_my_messages.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=20, pady=20)
+        self.messages = [self._create_message_widget(message, i) for i, message in enumerate(message_list)]
+        # pack the frame
+        self.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+    def _create_message_widget(self, message, i):
+        from_user = tk.Label(self, text=f"From: {message.from_user}", bg=self.bg, fg=self.text_color)
+        from_user.grid(row=i + 1, column=0, sticky="w", padx=10, pady=10)
+        send_at = tk.Label(self, text=f"Send at: {message.send_at}", bg=self.bg, fg=self.text_color)
+        send_at.grid(row=i + 1, column=1, sticky="w", padx=10, pady=10)
+        # download button
+        download_button = tk.Button(self, text="Download", command=lambda: self._download_message(message))
+        download_button.grid(row=i + 1, column=2, sticky="e", padx=10, pady=10)
+        logging.info(f"Message {message} created")
+
+    def _download_message(self, message: UserMessage):
+        logging.info(f"Download message {message.file_uuid}")
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Login/Registration")
@@ -58,5 +89,12 @@ if __name__ == "__main__":
     app = HeaderView(root, 800, 100, "John", bg=BLACK, text_color=WHITE)
     side_view = SideView(root, 200, 700, ["John", "Jane", "Jack"],
                          bg=BLACK, text_color=WHITE)
+    message_list = [
+        UserMessage("Jane", "John", datetime.now(), "Hello", "file_type", "session key"),
+        UserMessage("Jane1", "John1", datetime.now(), "Hello1", "file_type", "session key"),
+        UserMessage("Jane2", "John2", datetime.now(), "Hello2", "file_type", "session key"),
+        UserMessage("Jane3", "John3", datetime.now(), "Hello3", "file_type", "session key"),
+    ]
+    message_view = MessageView(root, 600, 700, bg=WHITE, text_color=BLACK, message_list=message_list)
     root.mainloop()
     logging.info("Exiting")
