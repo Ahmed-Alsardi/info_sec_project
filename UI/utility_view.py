903091be10e26349215393a01ae4b5dee80d05e9
Ahmed AlSardi <A7Dpy@outlook.com>
2022-04-28 20:15:14 +0300

    adding send file functionality

--- UI/utility_view.py
+++ UI/utility_view.py
@@ -1,9 +1,11 @@
-import tkinter as tk
 import logging
-from UI.utility import BLACK, WHITE
-from typing import List
-from application_context import UserMessage
+import tkinter as tk
 from datetime import datetime
+from tkinter.filedialog import askopenfile
+from typing import List
+
+from UI.utility import BLACK, WHITE
+from application_context import UserMessage
 
 logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
 
@@ -81,6 +83,62 @@ class MessageView(tk.Frame):
         logging.info(f"Download message {message.file_uuid}")
 
 
+class SendMessageView(tk.Frame):
+    def __init__(self, parent, width: int, height: int, bg: str, text_color: str, username: str):
+        tk.Frame.__init__(self, parent, width=width, height=height, background=bg)
+        self.file_path = None
+        self.frame_width = width
+        self.frame_height = height
+        self.bg = bg
+        self.text_color = text_color
+        self.username = username
+        self.__supported_file_types = [
+            ("Text Files", "*.txt"),
+            ("Image Files", "*.jpeg"),
+            ("PDF Files", "*.pdf"),
+        ]
+        # create label "send message to"
+        self.label_send_message_to = tk.Label(self, text=f"Send message to: {username}", bg=bg, fg=text_color)
+        self.label_send_message_to.grid(row=0, column=0, sticky="w", padx=20, pady=20)
+        # upload file
+        self.upload_file_label = tk.Label(self, text="Upload file", bg=bg, fg=text_color)
+        self.upload_file_label.grid(row=1, column=0, sticky="w", padx=20, pady=20)
+        # create button to upload file
+        self.upload_file_button = tk.Button(self, text="Upload", command=lambda: self.open_file())
+        self.upload_file_button.grid(row=1, column=1, sticky="e", padx=20, pady=20)
+
+        # pack the frame
+        self.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+    def open_file(self):
+        file_path = askopenfile(mode="r", filetypes=self.__supported_file_types)
+        if file_path is None:
+            return
+        print(type(file_path))
+        print(dir(file_path))
+        print(file_path)
+        logging.info(f"File {file_path.name} selected")
+        self.file_path = file_path
+        self.upload_label = tk.Label(self,
+                                     text=f"File {self._extract_file_name(file_path.name)} selected",
+                                     bg=self.bg, fg=self.text_color)
+        self.upload_label.grid(row=2, column=0, sticky="w", padx=20, pady=20)
+        self.upload_button = tk.Button(self, text="Send", command=lambda: self.upload_file())
+        self.upload_button.grid(row=3, column=0, sticky="e", padx=20, pady=20)
+
+    def upload_file(self):
+        if self.file_path is None:
+            tk.Label(self, text="Please select file first",
+                     bg=self.bg, fg=self.text_color).grid(row=3, column=0, sticky="w", padx=20, pady=20)
+            return
+        logging.info(f"File uploaded")
+        tk.Label(self, text="File uploaded",
+                 bg=self.bg, fg=self.text_color).grid(row=3, column=0, sticky="w", padx=20, pady=20)
+
+    def _extract_file_name(self, name):
+        return name.split("/")[-1]
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Login/Registration")
@@ -95,6 +153,7 @@ if __name__ == "__main__":
         UserMessage("Jane2", "John2", datetime.now(), "Hello2", "file_type", "session key"),
         UserMessage("Jane3", "John3", datetime.now(), "Hello3", "file_type", "session key"),
     ]
-    message_view = MessageView(root, 600, 700, bg=WHITE, text_color=BLACK, message_list=message_list)
+    # message_view = MessageView(root, 600, 700, bg=WHITE, text_color=BLACK, message_list=message_list)
+    send_message_view = SendMessageView(root, 600, 700, bg=WHITE, text_color=BLACK, username="John")
     root.mainloop()
     logging.info("Exiting")
